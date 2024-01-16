f1c9fac49c80832511cbc83e98879c875e7d31c1
XuboZheng <HZhertz76@gmail.com>
2024-01-16 19:43:04 +0800

    feat: 新增首页热门推荐

---
+++ src/pages/index/components/HotPanel.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+import type { HotItem } from '@/types/home'
+
+// 定义 props 接收数据
+defineProps<{
+  list: HotItem[]
+}>()
+</script>
+
+<template>
+  <!-- 推荐专区 -->
+  <view class="panel hot">
+    <view class="item" v-for="item in list" :key="item.id">
+      <view class="title">
+        <text class="title-text">{{ item.title }}</text>
+        <text class="title-desc">{{ item.alt }}</text>
+      </view>
+      <navigator hover-class="none" :url="`/pages/hot/hot?type=${item.type}`" class="cards">
+        <image
+          v-for="src in item.pictures"
+          :key="src"
+          class="image"
+          mode="aspectFit"
+          :src="src"
+        ></image>
+      </navigator>
+    </view>
+  </view>
+</template>
+
+<style lang="scss">
+@import '../styles/hot.scss';
+</style>
--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,9 +1,10 @@
 <script setup lang="ts">
 import { ref } from 'vue'
-import { getHomeBannerAPI, getHomeCategoryAPI } from '@/services/home'
-import type { BannerItem, CategoryItem } from '@/types/home'
+import { getHomeBannerAPI, getHomeCategoryAPI, getHomeHotAPI } from '@/services/home'
+import type { BannerItem, CategoryItem, HotItem } from '@/types/home'
 import CustomNavbar from './components/CustomNavbar.vue'
 import CategoryPanel from './components/CategoryPanel.vue'
+import HotPanel from './components/HotPanel.vue'
 import { onLoad } from '@dcloudio/uni-app'
 
 // 获取轮播图数据
@@ -20,13 +21,20 @@ const getHomeCategoryData = async () => {
   categoryList.value = res.result
 }
 
+// 获取热门推荐数据
+const hotList = ref<HotItem[]>([])
+const getHomeHotData = async () => {
+  const res = await getHomeHotAPI()
+  hotList.value = res.result
+}
+
 // 是否加载中标记
 const isLoading = ref(false)
 
 // 页面加载
 onLoad(async () => {
   isLoading.value = true
-  await Promise.all([getHomeBannerData(), getHomeCategoryData()])
+  await Promise.all([getHomeBannerData(), getHomeCategoryData(), getHomeHotData()])
   isLoading.value = false
 })
 </script>
@@ -38,6 +46,8 @@ onLoad(async () => {
   <XtxSwiper :list="bannerList" />
   <!-- 分类面板 -->
   <CategoryPanel :list="categoryList" />
+  <!-- 热门推荐 -->
+  <HotPanel :list="hotList" />
   <view class="index">index</view>
 </template>
 
