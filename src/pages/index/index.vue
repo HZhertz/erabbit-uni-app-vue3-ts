1f0e7caae62c10f329034ffb8b9fe4759a20709f
XuboZheng <HZhertz76@gmail.com>
2024-01-16 18:06:03 +0800

    feat: 新增首页轮播图

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,10 +1,33 @@
 <script setup lang="ts">
+import { ref } from 'vue'
+import { getHomeBannerAPI } from '@/services/home'
+import type { BannerItem } from '@/types/home'
 import CustomNavbar from './components/CustomNavbar.vue'
+// import XtxSwiper from '@/components/XtxSwiper.vue'
+import { onLoad } from '@dcloudio/uni-app'
+
+// 获取轮播图数据
+const bannerList = ref<BannerItem[]>([])
+const getHomeBannerData = async () => {
+  const res = await getHomeBannerAPI()
+  bannerList.value = res.result
+}
+
+// 是否加载中标记
+const isLoading = ref(false)
+
+// 页面加载
+onLoad(async () => {
+  isLoading.value = true
+  await Promise.all([getHomeBannerData()])
+  isLoading.value = false
+})
 </script>
 
 <template>
   <!-- 自定义导航栏 -->
   <CustomNavbar />
+  <XtxSwiper :list="bannerList" />
   <view class="index">index</view>
 </template>
 
--- src/types/component.d.ts
+++ src/types/component.d.ts
@@ -1,11 +1,7 @@
-/**
- * declare module '@vue/runtime-core'
- *   现调整为
- * declare module 'vue'
- */
-import 'vue'
-declare module 'vue' {
+import XtxSwiper from '@/components/XtxSwiper.vue'
+
+declare module '@vue/runtime-core' {
   export interface GlobalComponents {
-    //
+    XtxSwiper: typeof XtxSwiper
   }
 }
