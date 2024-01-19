506dbbbb61e178a45d7dc516101e130856ed9daa
XuboZheng <HZhertz76@gmail.com>
2024-01-19 18:36:35 +0800

    feat: 新增商品分类页

--- src/pages/category/category.vue
+++ src/pages/category/category.vue
@@ -1,11 +1,98 @@
 <script setup lang="ts">
-//
+import { getCategoryTopAPI } from '@/services/category'
+import { getHomeBannerAPI } from '@/services/home'
+import type { CategoryTopItem } from '@/types/category'
+import type { BannerItem } from '@/types/home'
+import { onLoad } from '@dcloudio/uni-app'
+import { computed, ref } from 'vue'
+import PageSkeleton from './components/PageSkeleton.vue'
+
+// 获取轮播图数据
+const bannerList = ref<BannerItem[]>([])
+const getBannerData = async () => {
+  const res = await getHomeBannerAPI(2)
+  bannerList.value = res.result
+}
+
+// 获取分类列表数据
+const categoryList = ref<CategoryTopItem[]>([])
+const activeIndex = ref(0)
+const getCategoryTopData = async () => {
+  const res = await getCategoryTopAPI()
+  categoryList.value = res.result
+}
+
+// 是否数据加载完毕
+const isFinish = ref(false)
+// 页面加载
+onLoad(async () => {
+  await Promise.all([getBannerData(), getCategoryTopData()])
+  isFinish.value = true
+})
+
+// 提取当前二级分类数据
+const subCategoryList = computed(() => {
+  return categoryList.value[activeIndex.value]?.children || []
+})
 </script>
 
 <template>
-  <view class="category">category</view>
+  <view class="viewport" v-if="isFinish">
+    <!-- 搜索框 -->
+    <view class="search">
+      <view class="input">
+        <text class="icon-search">女靴</text>
+      </view>
+    </view>
+    <!-- 分类 -->
+    <view class="categories">
+      <!-- 左侧：一级分类 -->
+      <scroll-view class="primary" scroll-y>
+        <view
+          v-for="(item, index) in categoryList"
+          :key="item.id"
+          class="item"
+          :class="{ active: index === activeIndex }"
+          @tap="activeIndex = index"
+        >
+          <text class="name">
+            {{ item.name }}
+          </text>
+        </view>
+      </scroll-view>
+      <!-- 右侧：二级分类 -->
+      <scroll-view enable-back-to-top class="secondary" scroll-y>
+        <!-- 焦点图 -->
+        <XtxSwiper class="banner" :list="bannerList" />
+        <!-- 内容区域 -->
+        <view class="panel" v-for="item in subCategoryList" :key="item.id">
+          <view class="title">
+            <text class="name">{{ item.name }}</text>
+            <navigator class="more" hover-class="none">全部</navigator>
+          </view>
+          <view class="section">
+            <navigator
+              v-for="goods in item.goods"
+              :key="goods.id"
+              class="goods"
+              hover-class="none"
+              :url="`/pages/goods/goods?id=${goods.id}`"
+            >
+              <image class="image" :src="goods.picture"></image>
+              <view class="name ellipsis">{{ goods.name }}</view>
+              <view class="price">
+                <text class="symbol">¥</text>
+                <text class="number">{{ goods.price }}</text>
+              </view>
+            </navigator>
+          </view>
+        </view>
+      </scroll-view>
+    </view>
+  </view>
+  <PageSkeleton v-else />
 </template>
 
 <style lang="scss">
-//
+@import './styles/category.scss';
 </style>
