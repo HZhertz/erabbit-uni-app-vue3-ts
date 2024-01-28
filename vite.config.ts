7dd87114bf0e9788f528f809a4c7ccfb29d191c5
XuboZheng <HZhertz76@gmail.com>
2024-01-28 15:53:28 +0800

    init:

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import uni from '@dcloudio/vite-plugin-uni'
 export default defineConfig({
   build: {
     // 开发阶段启用源码映射：https://uniapp.dcloud.net.cn/tutorial/migration-to-vue3.html#需主动开启-sourcemap
-    sourcemap: process.env.NODE_ENV === 'development',
+    sourcemap: process.env.NODE_ENV === 'development'
   },
-  plugins: [uni()],
+  plugins: [uni()]
 })
